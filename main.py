aab18fc54f0a4e7de03c022c9994a38e44fe3a5d
Summ3rs <jeremiassaguiar@gmail.com>
2025-08-03 15:43:00 -0300

    Adiciona nova versão do bot

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 import sys
 import os
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-
-from keep_alive import keep_alive
-keep_alive()
-
 import discord
 from discord.ext import commands
 import json
